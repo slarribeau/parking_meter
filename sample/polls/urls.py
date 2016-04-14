2acafaf9935f4a311dd2302966d34a21e439ccfe
Scott Larribeau <slarribeau@gmail.com>
2016-04-14 17:01:22 -0500

    REST paramater is meter_id instead of primary key

--- sample/polls/urls.py
+++ sample/polls/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^meters/$', views.meter_list),
-    url(r'^meters/(?P<pk>[0-9]+)/$', views.meter_detail),
+    url(r'^meters/(?P<pk>.*)/$', views.meter_detail),
 
 ]
 
--- sample/polls/views.py
+++ sample/polls/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
@@ -10,15 +7,8 @@ from polls.models import Meter
 from polls.serializers import MeterSerializer
 
 
-
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
 
 @csrf_exempt
 def meter_list(request):
@@ -31,17 +21,12 @@ def meter_list(request):
 @csrf_exempt
 def meter_detail(request, pk):
     print("meter detail" + pk);
-    meter = Meter.objects.get(pk=pk)
+    meter = Meter.objects.get(meter_id__exact=pk)
     serializer = MeterSerializer(meter)
     return JSONResponse(serializer.data)
 
-
-
-
-
-
-
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
+class JSONResponse(HttpResponse):
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
