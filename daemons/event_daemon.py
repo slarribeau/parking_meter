a36fcb3623431dd28905596ee6afa8a685396f23
Scott Larribeau <slarribeau@gmail.com>
2016-04-15 00:23:42 -0500

    Connected this version of code with the daemon script to run every minute

--- daemons/event_daemon.py
+++ daemons/event_daemon.py
@@ -20,22 +20,30 @@ def loop():
 
 
    processed = 0;
+   dup = 0;
    for each in raw_json:
          processed = processed + 1;
-
          ordinal = each.get('ordinal', 'xxx');
-         event_id = each.get('event_id', 'xxx');
-         event_time = each.get('event_time', 'xxx');
-         event_type = each.get('event_type', 'xxx');
-         meter_id = each.get('meter_id', 'xxx');
-         session_id = each.get('session_id', 'xxx');
-         cur.execute("""SELECT * from polls_meter WHERE meter_id = '%s';""" % (meter_id));
-         result=cur.fetchone();
-         print("""INSERT INTO polls_events (event_id, event_time, event_type, ordinal, session_id, meter_id_id) VALUES ('%s', '%s', '%s', '%s', '%s', '%s');""" % (event_id, event_time, event_type, ordinal, session_id, result[0]));
-         cur.execute("""INSERT INTO polls_events (event_id, event_time, event_type, ordinal, session_id, meter_id_id) VALUES ('%s', '%s', '%s', '%s', '%s', '%s');""" % (event_id, event_time, event_type, ordinal, session_id, result[0]));
 
-         conn.commit()
+         cur.execute("""SELECT COUNT(ordinal) from polls_events WHERE ordinal = '%s';""" % (ordinal));
+         result=cur.fetchone();
+         if (result[0] != 0) :
+            #print("match", ' ', ordinal);
+            dup = dup + 1;
+         else :
+            print("no match");
+            event_id = each.get('event_id', 'xxx');
+            event_time = each.get('event_time', 'xxx');
+            event_type = each.get('event_type', 'xxx');
+            meter_id = each.get('meter_id', 'xxx');
+            session_id = each.get('session_id', 'xxx');
+            cur.execute("""SELECT * from polls_meter WHERE meter_id = '%s';""" % (meter_id));
+            result=cur.fetchone();
+            print("""INSERT INTO polls_events (event_id, event_time, event_type, ordinal, session_id, meter_id_id) VALUES ('%s', '%s', '%s', '%s', '%s', '%s');""" % (event_id, event_time, event_type, ordinal, session_id, result[0]));
+            cur.execute("""INSERT INTO polls_events (event_id, event_time, event_type, ordinal, session_id, meter_id_id) VALUES ('%s', '%s', '%s', '%s', '%s', '%s');""" % (event_id, event_time, event_type, ordinal, session_id, result[0]));
+            conn.commit();
 
    print "PROCESSED from API: %d" % (processed)
+   print "DUP from API: %d" % (dup)
 
 loop();
